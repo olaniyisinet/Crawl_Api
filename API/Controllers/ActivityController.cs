01676ca29dd2fafbd275dc7bb4d6b177d8ba7b74
Olaniyi Olatunji <Olaniyi.Olatunji@sterlingbankng.com>
2018-09-18 07:18:33 +0100

    added log controller

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using API.Helper;
+using API.Models;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/Activity")]
+    public class ActivityController : ApiController
+    {
+        // POST api/Activity/LogError
+        [Route("LogError")]
+        public async Task<IHttpActionResult> LogError(ActivityModel model)
+        {
+            string log = Logger.LogError(model.User, model.Data);
+            return Ok(log);
+        }
+
+        // POST api/Activity/LogActivity
+        [Route("LogActivity")]
+        public async Task<IHttpActionResult> LogActivity(ActivityModel model)
+        {
+            string log = Logger.LogActivity(model.User, model.Data);
+            return Ok(log);
+        }
+    }
+}
---
+++ API/Helper/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+
+namespace API.Helper
+{
+    public static class Logger
+    {
+        private static readonly ILog _errorLogger = LogManager.GetLogger("ErrorFile");
+        private static readonly ILog _activityLogger = LogManager.GetLogger("ActivityFile");
+
+        public static string LogError(string user, string error)
+        {
+            try
+            {
+                _errorLogger.Error(user + "    :    " + DateTime.Now + "     :   " + error + "\n\n");
+                return "Logged";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static string LogActivity(string user, string activity)
+        {
+            try
+            {
+                _errorLogger.Info(user + "    :    " + DateTime.Now + "     :   " + activity + "\n\n");
+                return "Logged";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
